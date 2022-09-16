fa22f5982c76a21933fb15f18ddd72fa6a07e8af
eduzgs <eduardo.zgs@gmail.com>
2022-09-16 11:11:29 -0300

    estilos funcionando

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/global.scss';// no me funcionan los estilos, quizas estoy fallando en el
-// webpack.config,js, no carga el global.scss ):
+import './styles/global.scss';
 
 ReactDOM.render( <App /> ,document.getElementById('app'));
